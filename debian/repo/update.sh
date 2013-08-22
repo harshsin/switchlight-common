58dfe9319e3bdb2e051855d72e250dfad58a1eac
Rob Sherwood <rob.sherwood@bigswitch.com>
2013-08-22 21:56:41 +0000

    Added a quick script to rsync deb repo from switch-nfs
    Convenience script for managing new awesome build system.

---
+++ debian/repo/update.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+if [ -z $SERVER ] ; then
+	SERVER=switch-nfs.bigswitch.com
+fi
+if [ -z $BRANCH ]; then
+	BRANCH=master
+fi
+if [ -z $BUILD ]; then
+	BUILD=latest
+fi
+
+echo Copying files from host=$SERVER branch=$BRANCH build=$BUILD
+
+# NOTE the trailing / is important here
+rsync -av $USER@${SERVER}:/var/export/switchlight/autobuilds/$BRANCH/$BUILD/repo/ .
