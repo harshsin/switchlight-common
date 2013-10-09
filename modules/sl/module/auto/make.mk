a3182fec25c0ba68d2a03eac24860f5360a8084d
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2013-10-09 16:34:57 -0700

    Moved to submodules/SL and organized as a separate module repository.

--- modules/sl/module/auto/make.mk
+++
@@ -1,9 +0,0 @@
-###############################################################################
-#
-# sl Autogeneration
-#
-###############################################################################
-sl_AUTO_DEFS := module/auto/sl.yml
-sl_AUTO_DIRS := module/inc/sl module/src
-include $(BUILDER)/auto.mk
-
