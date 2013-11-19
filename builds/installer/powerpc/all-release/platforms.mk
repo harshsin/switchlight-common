da5c8f1a2832f0bb3b19dc467b9ee2ee671d72b0
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2013-11-19 11:43:02 -0800

    Add accton.

--- builds/installer/powerpc/all-release/platforms.mk
+++ builds/installer/powerpc/all-release/platforms.mk
@@ -1,1 +1,1 @@
-INSTALLER_PLATFORMS := quanta-lb9 quanta-ly2 quanta-ly2r quanta-lb8d quanta-ly5 delta-7448 celestica-redstone
+INSTALLER_PLATFORMS := quanta-lb9 quanta-ly2 quanta-ly2r quanta-lb8d quanta-ly5 delta-7448 celestica-redstone accton-es5652bt
