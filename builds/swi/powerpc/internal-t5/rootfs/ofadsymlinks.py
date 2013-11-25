21ee6704801116ee0bb390db113889699fa919a2
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2013-11-25 08:04:07 -0800

    Custom ofad symlinks.

---
+++ builds/swi/powerpc/internal-t5/rootfs/ofadsymlinks.py
@@ -0,0 +1,5 @@
+import os
+os.symlink("ofad-6.2-t5", "/usr/bin/ofad")
+os.symlink("linux-kernel-bde-6.2.ko", "/lib/modules/linux-kernel-bde.ko")
+os.symlink("linux-user-bde-6.2.ko", "/lib/modules/linux-user-bde.ko")
+
