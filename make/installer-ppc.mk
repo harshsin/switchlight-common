70c022fb56d0a7719ddbe9e5a621b9f2108f688b
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2013-12-20 10:40:03 -0800

    - Updated for platform-config packages. - Now the default installer for all platforms.

--- make/installer-ppc.mk
+++ make/installer-ppc.mk
@@ -23,12 +23,9 @@ ifndef INSTALLER_NAME
 $(error $$INSTALLER_NAME is not set)
 endif
 
-# Each platform flash template comes from the platform debian package:
-# (not every platform includes every file)
-PLATFORM_FLASH_TEMPLATES := $(foreach p,$(INSTALLER_PLATFORMS),$(shell $(SWITCHLIGHT_PKG_INSTALL) platform-$p:powerpc --find-file switchlight.$(p).loader) $(shell $(SWITCHLIGHT_PKG_INSTALL) platform-$p:powerpc --find-file switchlight.$(p).flash) $(shell $(SWITCHLIGHT_PKG_INSTALL) platform-$p:powerpc --find-file switchlight.$(p).jffs2))
-
-# These are the basenames of the flash templates. 
-PLATFORM_FLASH_TEMPLATE_NAMES := $(foreach p,$(PLATFORM_FLASH_TEMPLATES),$(notdir $(p)))
+# Get the platform loaders from each platform package
+PLATFORM_LOADERS := $(foreach p,$(INSTALLER_PLATFORMS),$(shell $(SWITCHLIGHT_PKG_INSTALL) platform-$(p):powerpc --find-file switchlight.$(p).loader))
+# Get the platform config package for each platform
 
 $(INSTALLER_NAME): $(INSTALLER_NAME).cpio
 	$(SL_V_at)cp /dev/null $@
@@ -39,15 +36,16 @@ $(INSTALLER_NAME): $(INSTALLER_NAME).cpio
 	$(SL_V_GEN)gzip -9 < $@.cpio >> $@
 	$(SL_V_at)rm $@.cpio
 
-$(INSTALLER_NAME).cpio: $(PLATFORM_FLASH_TEMPLATES) $(INSTALLER_SWI)
-	$(SL_V_at)cp $(PLATFORM_FLASH_TEMPLATES) .
+$(INSTALLER_NAME).cpio: $(PLATFORM_DIRS) $(INSTALLER_SWI)
+	$(SL_V_at)cp $(PLATFORM_LOADERS) .
+	$(foreach p,$(INSTALLER_PLATFORMS), $(SWITCHLIGHT_PKG_INSTALL) platform-config-$(p):all --extract .;)
 	$(SL_V_at)cp $(INSTALLER_SWI) switchlight-powerpc.swi
 	$(SL_V_GEN)set -o pipefail ;\
 	if $(SL_V_P); then v="-v"; else v="--quiet"; fi ;\
-	find $(PLATFORM_FLASH_TEMPLATE_NAMES) switchlight-powerpc.swi \
+	find *.loader lib switchlight-powerpc.swi \
 	| cpio $$v -H newc -o > $@
-	$(SL_V_at)rm -f *.jffs2 *.loader *.flash
 	$(SL_V_at)rm -f switchlight-powerpc.swi
+	$(SL_V_at)rm -rf ./lib ./usr *.loader
 
 $(INSTALLER_SWI):
 	$(MAKE) -C $(dir $(INSTALLER_SWI))
