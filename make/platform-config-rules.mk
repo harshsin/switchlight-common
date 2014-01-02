2853c0ddc1d2733d91301b76d2e8256b6896379f
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2014-01-02 14:52:45 -0800

    Pull platform-specific BRCM initialization scripts directly from the BRCM module.

--- make/platform-config-rules.mk
+++ make/platform-config-rules.mk
@@ -1,8 +1,12 @@
-# This file is included by the debian/rules file for all platform-config packages. 
+# This file is included by the debian/rules file for all platform-config packages.
 DEB_DH_INSTALL_SOURCEDIR=debian/tmp
 INSTALL_DIR=$(CURDIR)/$(DEB_DH_INSTALL_SOURCEDIR)
 
 BASEDIR=/lib/platform-config
+
+ifndef BRCM_PLATFORM_NAME
+BRCM_PLATFORM_NAME=$(PLATFORM_NAME)
+endif
 
 %:
 	dh $@
@@ -16,5 +20,7 @@ clean:
 override_dh_auto_install:
 	mkdir -p $(INSTALL_DIR)$(BASEDIR)/
 	cp -R ../../src $(INSTALL_DIR)$(BASEDIR)/$(PLATFORM_NAME)
+	cp -R $(SWITCHLIGHT_SUBMODULE_BROADCOM)/Modules/BRCM/module/src/platforms/$(BRCM_PLATFORM_NAME)/sbin/* $(INSTALL_DIR)$(BASEDIR)/$(PLATFORM_NAME)/sbin
+
 
 
