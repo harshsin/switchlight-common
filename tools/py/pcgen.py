5463c5ea7646b73fc973bc6061bc5b32021d8260
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2014-01-03 09:25:41 -0800

    Update autogenerated initial content.

--- tools/py/pcgen.py
+++ tools/py/pcgen.py
@@ -12,6 +12,7 @@ class PlatformConfigGenerator(ComponentGenerator):
     def __init__(self, platform, vendor):
         self.platform = platform
         self.vendor = vendor
+        self.Vendor = vendor.title()
         summary="Platform Configuration files for %s." % self.platform
         ComponentGenerator.__init__(self, platform, "platform-config-" + self.platform,
                                     "all", summary, summary)
@@ -29,10 +30,13 @@ class PlatformConfigGenerator(ComponentGenerator):
     def _rules(self):
         return """#!/usr/bin/make -f
 PLATFORM_NAME=%(platform)s
+BRCM_PLATFORM_NAME=none
 include $(SWITCHLIGHT)/make/platform-config-rules.mk
 """ % (self.__dict__)
 
     def __generate_file(self, path, name, contents):
+        if not os.path.exists(path):
+            os.makedirs(path)
         with open("%s/%s" % (path, name), "w") as f:
             f.write(contents)
 
@@ -46,11 +50,17 @@ include $(SWITCHLIGHT)/make/platform-config-rules.mk
         # the platform directory layout is this
         os.makedirs('%(path)s/src/install' % (self.__dict__))
         os.makedirs('%(path)s/src/boot' % (self.__dict__))
-
+        os.makedirs('%(path)s/src/sbin' % (self.__dict__))
+        os.makedirs('%(path)s/src/python' % (self.__dict__))
         self.__generate_file('%(path)s/src' % self.__dict__, 'name', self.platform+'\n')
         self.__generate_file('%(path)s/src/install' % self.__dict__,
                              '%(platform)s.sh' % self.__dict__,
                              "# Platform data goes here.")
+        self.__generate_file('%(path)s/src/sbin' % self.__dict__,
+                             'gpio_init',
+                             """# platform: %(platform)s
+exit 0
+""" % self.__dict__)
         self.__generate_file('%(path)s/src/boot' % self.__dict__,
                              self.platform,
                              "# Platform data goes here.")
@@ -65,15 +75,45 @@ else
 fi
 
 """ % self.__dict__)
+        self.__generate_file('%(path)s/src/python' % self.__dict__,
+                             'slpc.py',
+                             """#!/usr/bin/python
+############################################################
+#
+# Platform Driver for %(platform)s
+#
+############################################################
+import os
+import struct
+import time
+import subprocess
+from switchlight.platform.base import *
+from switchlight.vendor.quanta import *
+
+class SwitchLightPlatformImplementation(SwitchLightPlatform%(Vendor)s):
+
+    def model(self):
+        raise Exception()
+
+    def platform(self):
+        return '%(platform)s'
+
+    def _plat_info_dict(self):
+        raise Exception()
+
+    def oid_table(self):
+        raise Exception()
+
+
+if __name__ == "__main__":
+    print SwitchLightPlatformImplementation()
+
+""" % self.__dict__)
 
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
         print "usage: %s <platform-name> <vendor-name>" % sys.argv[0]
         sys.exit(1)
-
     pc = PlatformConfigGenerator(sys.argv[1], sys.argv[2])
     pc.generate('.')
-
-
-
