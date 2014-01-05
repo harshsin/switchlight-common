1cdf3924d828b2c0c208febc072c8a8e439262cb
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2014-01-05 06:35:47 -0800

    Add as4600-54t.

--- builds/installer/powerpc/bringup/platforms.mk
+++ builds/installer/powerpc/bringup/platforms.mk
@@ -1,3 +1,3 @@
-INSTALLER_PLATFORMS := powerpc-quanta-lb9-r0 powerpc-quanta-ly2-r0 powerpc-as5610-52x
+INSTALLER_PLATFORMS := powerpc-quanta-lb9-r0 powerpc-quanta-ly2-r0 powerpc-as5610-52x powerpc-as4600-54t
 
 
