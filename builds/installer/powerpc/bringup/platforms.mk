3f9eb693122eaebca2b7dc167263fde09a714e3c
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2014-01-14 14:01:21 -0800

    Add AS6700.

--- builds/installer/powerpc/bringup/platforms.mk
+++ builds/installer/powerpc/bringup/platforms.mk
@@ -1,3 +1,4 @@
-INSTALLER_PLATFORMS := powerpc-quanta-lb9-r0 powerpc-quanta-ly2-r0 powerpc-as5610-52x powerpc-as4600-54t
+INSTALLER_PLATFORMS := powerpc-quanta-lb9-r0 powerpc-quanta-ly2-r0 powerpc-as5610-52x powerpc-as4600-54t powerpc-as6700-32x-r0
+
 
 
