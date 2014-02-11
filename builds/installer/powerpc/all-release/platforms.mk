8178ed928c50df0dce90bf0dbd9f891ce01b6325
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2014-02-11 15:24:40 -0800

    Add AS6700 and AS5710.

--- builds/installer/powerpc/all-release/platforms.mk
+++ builds/installer/powerpc/all-release/platforms.mk
@@ -8,5 +8,8 @@
 # 
 # </bsn.cl>
 ############################################################
-INSTALLER_PLATFORMS := powerpc-quanta-lb9-r0 powerpc-quanta-ly2-r0 
+INSTALLER_PLATFORMS := powerpc-quanta-lb9-r0 powerpc-quanta-ly2-r0 powerpc-as6700-32x-r0 powerpc-as5710-54x-r0a
+
+
+
 
