2e7008da6cdff362ac2156248b123aca32053e04
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2014-02-17 10:56:03 -0800

    Make useful: - Local server authorizations - Whitelisted user account authorizations.

--- components/all/slrest/src/python/slrest/base/auth.py
+++ components/all/slrest/src/python/slrest/base/auth.py
@@ -4,22 +4,86 @@
 # Authentication support. 
 #
 ############################################################
-# Obviously temporary, just getting the scaffolding up.
-AUTH_DICT = {
+import crypt
+import spwd
+
+logger = None
+
+#
+# Optional Server Authorization Credentials. 
+#
+SERVER_AUTHORIZATIONS = {
     'admin' : 'bsn',
 }
 
-def checkpassword(realm, username, password):
-    return username in AUTH_DICT and AUTH_DICT[username] == password
-
-def clear():
+def server_auth_clear_all():
     """Clear existing authentication pairs."""
-    AUTH_DICT.clear()
+    SERVER_AUTHORIZATIONS.clear()
 
-
-def add(username, password):
+def server_auth_add(username, password):
     """Add to existing authentication pairs."""
-    AUTH_DICT[username] = password
+    SERVER_AUTHORIZATIONS[username] = password
+
+def server_auth_remove(username):
+    SERVER_AUTHORIZATIONS.pop(username, None)
+    
+
+def server_auth_check(username, password):
+    """Check a user against the local authorization dict."""
+    if username not in SERVER_AUTHORIZATIONS:
+        return None
+    return SERVER_AUTHORIZATIONS[username] == password
+
+
+
+#
+# Local account authorizations
+#
+# Only user accounts in the whitelist will be checked:
+USER_ACCOUNTS = {
+    'admin' : True, 
+    'root' : True,
+}
+
+def user_account_clear_all():
+    USER_ACCOUNTS.clear()
+
+def user_account_add(username):
+    USER_ACCOUNTS[username] = True
+
+def user_account_remove(username):
+    USER_ACCOUNTS.pop(username, None)
+
+def user_account_check(username, password):
+
+    if (username not in USER_ACCOUNTS or USER_ACCOUNTS[username] is False) and ('*' not in USER_ACCOUNTS):
+        # User is not enabled
+        return None
+
+    try:
+        hashedpw = spwd.getspnam(username)[1]
+    except KeyError, e:
+        # User does not exist
+        return None
+
+    if hashedpw in ["NP", "!", "", "LK", "*", "!!", None]:
+        # No password, account locked, or password expired. Do not allow. 
+        return False
+    
+    return crypt.crypt(password, hashedpw) == hashedpw
+
+
+#
+# Main authorizations routine. This is passed as the validator
+# to cherrypy. 
+#
+def checkpassword(realm, username, password):
+    if server_auth_check(username, password):
+        return True
+    if user_account_check(username, password):
+        return True
+
+    return False
 
 
 
