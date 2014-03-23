c81082e9db63d08879f0e6e607235f212557668a
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2014-03-23 08:26:24 -0700

    Initial support for the DNI 7448.

---
+++ components/all/platform-config/powerpc-dni-7448-r0/src/python/slpc.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+############################################################
+#
+# Platform Driver for powerpc-dni-7448-r0
+#
+############################################################
+import os
+import struct
+import time
+import subprocess
+from switchlight.platform.base import *
+from switchlight.vendor.dni import *
+
+class SwitchLightPlatformImplementation(SwitchLightPlatformDNI):
+
+    def model(self):
+        return "DNI 7448"
+
+    def platform(self):
+        return 'powerpc-dni-7448-r0'
+
+    def _plat_info_dict(self):
+        return {
+            platinfo.LAG_COMPONENT_MAX : 16,
+            platinfo.PORT_COUNT : 54
+            }
+
+    def _plat_oid_table(self):
+        return None
+
+    def get_environment(self):
+        return "Not implemented."
+
+
+
+if __name__ == "__main__":
+    from switchlight.platform.main import main
+    main(SwitchLightPlatformImplementation())
+
+
+
