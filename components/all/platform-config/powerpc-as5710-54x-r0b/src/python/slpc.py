aee2625094a07dd36cab54cb9dbd3ceff464e1c1
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2014-04-24 13:31:50 -0700

    Support new hardware revisions for the AS5710 and AS6700.

---
+++ components/all/platform-config/powerpc-as5710-54x-r0b/src/python/slpc.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+############################################################
+#
+# Platform Driver for powerpc-as5710-54x-r0b
+#
+############################################################
+import os
+import struct
+import time
+import subprocess
+from switchlight.platform.base import *
+from switchlight.vendor.accton import *
+
+class SwitchLightPlatformImplementation(SwitchLightPlatformAccton):
+
+    def model(self):
+        return "Accton AS5710-54X"
+
+    def platform(self):
+        return 'powerpc-as5710-54x-r0b'
+
+    def _plat_info_dict(self):
+        return {
+            platinfo.LAG_COMPONENT_MAX : 16,
+            platinfo.PORT_COUNT : 54
+            }
+
+    def _plat_oid_table(self):
+        return None
+
+    def get_environment(self):
+        return "Not implemented."
+
+
+
+if __name__ == "__main__":
+    from switchlight.platform.main import main
+    main(SwitchLightPlatformImplementation())
+
+
+
