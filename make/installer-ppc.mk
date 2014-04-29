eeed457da9321893ac4e81e9340a1e9c131d4d6b
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2014-04-29 15:30:50 -0700

    - Allow the installer package to provide a custom boot-config file. - Make the SWI optional during the installer build.

--- make/installer-ppc.mk
+++ make/installer-ppc.mk
@@ -13,10 +13,6 @@ endif
 ifndef INSTALLER_PLATFORMS
 $(error $$INSTALLER_PLATFORMS not defined)
 endif
-# The SWI to include in the installer must be specified
-ifndef INSTALLER_SWI
-$(error $$INSTALLER_SWI is not set)
-endif
 
 # The final name of the installer file must be specified.
 ifndef INSTALLER_NAME
@@ -32,23 +28,30 @@ ZTN_MANIFEST := zerotouch.json
 
 
 $(INSTALLER_NAME): $(PLATFORM_DIRS) $(INSTALLER_SWI) $(ZTN_MANIFEST)
+	rm -f *.installer
 	$(SL_V_at)cp $(PLATFORM_LOADERS) .
 	$(foreach p,$(INSTALLER_PLATFORMS), $(SWITCHLIGHT_PKG_INSTALL) platform-config-$(p):all --extract .;)
+ifdef INSTALLER_SWI
 	$(SL_V_at)cp $(INSTALLER_SWI) switchlight-powerpc.swi
+endif
 	$(SL_V_at)sed \
 	  -e 's^@SLVERSION@^$(RELEASE)^g' \
 	  $(SWITCHLIGHT)/builds/installer/installer.sh \
 	>> installer.sh
 	$(SL_V_GEN)set -o pipefail ;\
 	if $(SL_V_P); then v="-v"; else v="--quiet"; fi ;\
-	$(SWITCHLIGHT)/tools/mkshar --lazy $@ $(SWITCHLIGHT)/tools/sfx.sh.in installer.sh *.loader lib switchlight-powerpc.swi $(ZTN_MANIFEST)
-	$(SL_V_at)rm -f switchlight-powerpc.swi installer.sh
-	$(SL_V_at)rm -rf ./lib ./usr *.loader $(ZTN_MANIFEST)
+	$(SWITCHLIGHT)/tools/mkshar --lazy $@ $(SWITCHLIGHT)/tools/sfx.sh.in installer.sh *.loader lib switchlight-powerpc.swi $(ZTN_MANIFEST) $(INSTALLER_EXTRA_FILES)
+ifdef INSTALLER_SWI
+	$(SL_V_at)rm -f switchlight-powerpc.swi
+endif
+	$(SL_V_at)rm -rf installer.sh ./lib ./usr *.loader $(ZTN_MANIFEST)
 
 shar installer: $(INSTALLER_NAME)
 
+ifdef INSTALLER_SWI
 $(INSTALLER_SWI):
 	$(MAKE) -C $(dir $(INSTALLER_SWI))
+endif
 
 # Build config
 ifndef SWITCHLIGHT_BUILD_CONFIG
