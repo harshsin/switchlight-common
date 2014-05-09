7c3993b737a8a5c4cca0c10855c5c6113bdc4674
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2014-05-09 14:38:05 -0700

    Validation.

--- components/all/slrest/src/python/slrest/base/response.py
+++ components/all/slrest/src/python/slrest/base/response.py
@@ -32,12 +32,42 @@ class SLREST(object):
         EXPIRED="EXPIRED"
         MISSING="MISSING"
 
+        @classmethod
+        def valid(klass, s):
+            # Close enough
+            return s in klass.__dict__
+
     @staticmethod
-    def response(path, status, reason, transaction, data):
+    def response(path, status, reason=None, transaction=None, data=None):
         r = {}
+
+        if reason is None:
+            reason = status
+
         r[SLREST.Keys.PATH] = path
         r[SLREST.Keys.STATUS] = status
         r[SLREST.Keys.REASON] = reason
         r[SLREST.Keys.TRANSACTION] = transaction
         r[SLREST.Keys.DATA] = data
+
+        #
+        # Enforce protocol requirements.
+        # We don't throw an exception here to communicate it
+        # but hijack the message. This is mostly for developer
+        # testing.
+        #
+        ereasons = ''
+        if path is None:
+            ereasons += "The PATH key is None. This should be fixed."
+        if not SLREST.Status.valid(status):
+            ereasons += "The reported status value '%s' is invalid." % status
+
+        if len(ereasons):
+            # Return as an error messsage with the original content in data field:
+            e = {}
+            e[SLREST.Keys.STATUS] = SLREST.Status.ERROR
+            e[SLREST.Keys.REASON] = "SLREST Protocol Error. Reasons:" + ereasons + "The data key contains the original response."
+            e[SLREST.Keys.DATA] = r
+            r = e
+
         return json.dumps(r)
