2db38e093b78e9969093b743d0ec1084b195a093
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2014-05-20 13:48:44 -0700

    Support installation on the MMC card (Hardware workaround). - Some of our engineering samples have a (known) problem with the onboard flash.   I've opened these boxes up and inserted and MMC card as a replacement.   When the uboot variable "sl_installer_use_mmc" is set then we will   install on the MMC card instead of the flash.

--- components/all/platform-config/powerpc-as6700-32x-r0/src/install/powerpc-as6700-32x-r0.sh
+++ components/all/platform-config/powerpc-as6700-32x-r0/src/install/powerpc-as6700-32x-r0.sh
@@ -1,21 +1,30 @@
 ############################################################
 # <bsn.cl fy=2013 v=none>
-# 
-#        Copyright 2013, 2014 BigSwitch Networks, Inc.        
-# 
-# 
-# 
+#
+#        Copyright 2013, 2014 BigSwitch Networks, Inc.
+#
+#
+#
 # </bsn.cl>
 ############################################################
 ############################################################
 #
 # Installer scriptlet for the powerpc-as6700-32x
 #
-
-# The loader is installed in the fat partition of the first USB storage device
-platform_bootcmd='usb start; fatload usb 0:1 0x10000000 switchlight-loader; setenv bootargs console=$consoledev,$baudrate sl_platform=powerpc-as6700-32x-r0; bootm 0x10000000'
+if fw_printenv sl_installer_use_mmc; then
+    # The loader is installed in the fat partition of the first MMC device.
+    # This is a workaround for early samples that had a broken flash device.
+    # It requires manually inserted an SD card.
+    # This cannot be used in production.
+    platform_bootcmd='mmc part 0; fatload mmc 0:1 0x10000000 switchlight-loader; setenv bootargs console=$consoledev,$baudrate sl_platform=powerpc-as6700-32x-r0; bootm 0x10000000'
+    __blockdev=mmcblk0
+else
+    # The loader is installed in the fat partition of the first USB storage device
+    platform_bootcmd='usb start; fatload usb 0:1 0x10000000 switchlight-loader; setenv bootargs console=$consoledev,$baudrate sl_platform=powerpc-as6700-32x-r0; bootm 0x10000000'
+    __blockdev=sda
+fi
 
 platform_installer() {
     # Standard installation to usb storage
-    installer_standard_blockdev_install sda 16M 64M ""
+    installer_standard_blockdev_install "${__blockdev}" 16M 64M ""
 }
