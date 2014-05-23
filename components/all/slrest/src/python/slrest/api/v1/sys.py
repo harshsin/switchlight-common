af8fec68fc5bcc47537c7a1b9fe50a0775002e14
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2014-05-23 12:50:23 -0700

    Support generic access to transaction workspace files.

--- components/all/slrest/src/python/slrest/api/v1/sys.py
+++ components/all/slrest/src/python/slrest/api/v1/sys.py
@@ -60,3 +60,51 @@ class v1_sys_reboot(SLAPIObject):
         return SLREST.ok(self.route,
                          reason="Rebooting in %s seconds...\n" % delay)
 
+class v1_sys_file_syslog(SLAPIObject):
+    """Get the current syslog."""
+    route = "/api/v1/sys/file/syslog"
+    def GET(self, gzip, sync=False):
+
+        class GetSysLog(TransactionTask):
+            def handler(self):
+                self.files = {}
+                fid='syslog'
+                src='/var/log/syslog'
+                if self.args:
+                    # GZip the file first
+                    dst="%s/result.gz" % self.workdir
+                    (rc, out) = util.bash_command("gzip -c %s > %s" % (src,dst))
+                    if rc:
+                        self.status = SLREST.Status.ERROR
+                        self.reason = out
+                    else:
+                        self.files['syslog'] = (os.path.abspath(dst),
+                                                'application/gzip')
+                        self.status = SLREST.Status.OK
+                        self.data=self.fidurl(fid);
+                else:
+                    self.files['syslog'] = (src,
+                                            'application/text')
+                    self.status = SLREST.Status.OK
+                    self.data=self.fidurl(fid);
+
+                self.finish()
+
+
+        (gzip, error) = params.boolean('gzip', gzip)
+        if error:
+            return SLREST.error(self.route, reason=error)
+
+        tm = TransactionManagers.get("SYS")
+        (tid, tt) = tm.new_task(GetSysLog, self.route, gzip)
+        if sync:
+            tt.join()
+        return tt.response()
+
+
+
+
+
+
+
+
--- components/all/slrest/src/python/slrest/api/v1/transact.py
+++ components/all/slrest/src/python/slrest/api/v1/transact.py
@@ -22,28 +22,45 @@ class v1_transaction(SLAPIObject):
         if tt:
             return tt.response()
         else:
-            return SLREST.response(self.route, SLREST.Status.MISSING)
+            return SLREST.missing(self.route)
 
 class v1_transactions_running(SLAPIObject):
     """Show all running transaction ids"""
     route = "/api/v1/transactions/running"
     def GET(self):
-        return SLREST.response(self.route, SLREST.Status.OK,
-                               data=TransactionManagers.get_tids_running())
+        return SLREST.ok(self.route,
+                         data=TransactionManagers.get_tids_running())
 
 class v1_transactions_finished(SLAPIObject):
     """Show all finished transaction ids"""
     route = "/api/v1/transactions/finished"
     def GET(self):
-        return SLREST.response(self.route, SLREST.Status.OK,
-                               data=TransactionManagers.get_tids_finished())
+        return SLREST.ok(self.route,
+                         data=TransactionManagers.get_tids_finished())
 
 class v1_transactions_all(SLAPIObject):
     """Show all transactions"""
     route = "/api/v1/transactions/all"
     def GET(self):
-        return SLREST.response(self.route, SLREST.Status.OK,
-                               data=TransactionManagers.get_tids_all())
+        return SLREST.ok(self.route,
+                         data=TransactionManagers.get_tids_all())
 
 
+class v1_transaction_file(SLAPIObject):
+    """Retrieve a transaction file."""
+    route = "/api/v1/transactions/file"
+    def GET(self, id, fid):
+        tt = TransactionManagers.get_task(id)
+        if tt:
+            try:
+                (filename, content_type) = tt.files[fid]
+                result = cherrypy.lib.static.serve_file(
+                    filename, content_type=content_type,
+                    disposition='attachment',
+                    name=os.path.basename(filename))
+                return result
+            except (AttributeError, KeyError), e:
+                return SLREST.missing(self.route, "The file is not recognized for this transaction.")
+        else:
+            return SLREST.missing(self.route, "The transaction id does not exist.")
 
