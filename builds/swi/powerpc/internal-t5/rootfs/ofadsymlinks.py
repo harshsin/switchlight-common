2b0f1f0ebed8363558172dcea581f1e6abfb6b8f
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2014-05-27 12:07:53 -0700

    Rename ofad-6.3.3-t5 as ofad-6.3.3-internal-t5.

--- builds/swi/powerpc/internal-t5/rootfs/ofadsymlinks.py
+++ builds/swi/powerpc/internal-t5/rootfs/ofadsymlinks.py
@@ -1,5 +1,3 @@
 import os
-os.symlink("ofad-6.3-t5", "/usr/bin/ofad")
-os.symlink("linux-kernel-bde-6.3.ko", "/lib/modules/linux-kernel-bde.ko")
-os.symlink("linux-user-bde-6.3.ko", "/lib/modules/linux-user-bde.ko")
+os.symlink("ofad-6.3-internal-t5", "/usr/bin/ofad")
 
