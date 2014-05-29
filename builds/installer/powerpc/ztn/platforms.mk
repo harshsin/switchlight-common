955d6b75e0c75a3aa974cc80a259250500f13120
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2014-05-29 15:51:01 -0700

    Dead.

--- builds/installer/powerpc/ztn/platforms.mk
+++ builds/installer/powerpc/ztn/platforms.mk
@@ -8,7 +8,7 @@
 #
 # </bsn.cl>
 ############################################################
-INSTALLER_PLATFORMS := powerpc-quanta-lb9-r0 powerpc-quanta-ly2-r0 powerpc-dni-7448-r0 powerpc-as6700-32x-r0 powerpc-as5710-54x-r0a powerpc-as6700-32x-r0b powerpc-as5710-54x-r0b
+INSTALLER_PLATFORMS := powerpc-quanta-lb9-r0 powerpc-quanta-ly2-r0 powerpc-dni-7448-r0 powerpc-as6700-32x-r0 powerpc-as5710-54x-r0a powerpc-as5710-54x-r0b
 
 
 
