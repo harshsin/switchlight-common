b72acd315f24c2378e94865ea0c607e1c855bb09
Harshmeet Singh <harshmeet.singh@bigswitch.com>
2014-06-05 15:12:40 -0700

    infra support for slrest client

--- components/all/slrest/src/bin/slrest.py
+++ components/all/slrest/src/bin/slrest.py
@@ -6,5 +6,23 @@
 ############################################################
 #
 #
-print "Harshmeet should implement me soon."
 
+from slrest.base.slapi_object import SLAPIObject
+from slrest.api import *
+from slrest.api.v1 import *
+
+import sys
+import argparse
+import logging
+
+logger = logging.getLogger("slrest-cli")
+logger.setLevel(logging.DEBUG)
+parser = argparse.ArgumentParser(description='CLI Interface to the SLREST APIs',
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--hostname", default="localhost", help="Device running slrest server")
+parser.add_argument("--port", default="")
+sub_parser = parser.add_subparsers()
+SLAPIObject.mount_all(logger)
+SLAPIObject.mount_cmds(sub_parser)
+args = parser.parse_args()
+args.func(args)
