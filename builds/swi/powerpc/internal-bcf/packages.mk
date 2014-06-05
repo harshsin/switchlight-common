83552a66af3f9982fa090233f0a3153727031d45
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2014-06-05 15:27:11 -0700

    Package configurations.

---
+++ builds/swi/powerpc/internal-bcf/packages.mk
@@ -0,0 +1,25 @@
+############################################################
+#
+#
+############################################################
+include $(SWITCHLIGHT)/builds/swi/powerpc/basepackages.mk
+
+# Share the same platform list with the release configuration
+include ../release-bcf/platforms.mk
+
+SWI_PACKAGES_ALL += pcli slrest switchlight-internal-dbg-sources t6-dump-flows
+
+SWI_PACKAGES_POWERPC += \
+	brcmd-6.3.3-internal \
+	libbroadcom-6.3-internal-dbg \
+	libbroadcom-6.3-kernel-85xx-modules \
+	libbroadcom-6.3-kernel-as-e500mc-modules \
+	ofad-6.3.3-internal-t5 \
+	sofcon \
+	orc-brcm-6.3.3-internal \
+	orc
+
+
+
+
+
--- builds/swi/powerpc/release-bcf/platforms.mk
+++ builds/swi/powerpc/release-bcf/platforms.mk
@@ -1,4 +1,4 @@
-ZTN_PLATFORMS := powerpc-quanta-ly2-r0 \
+SWI_PLATFORMS := powerpc-quanta-ly2-r0 \
 	         powerpc-as6700-32x-r0 \
 		 powerpc-as5710-54x-r0a \
 		 powerpc-as5710-54x-r0b
