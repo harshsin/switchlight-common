24bf71c8502b298be2715a73a0027b739266338d
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2014-06-05 15:59:33 -0700

    - Import Accton platform code. - Set package dependencies properly.

--- components/all/platform-config/powerpc-as4600-54t/src/python/slpc.py
+++ components/all/platform-config/powerpc-as4600-54t/src/python/slpc.py
@@ -8,7 +8,6 @@
 # 
 # </bsn.cl>
 ############################################################
-############################################################
 #
 # Platform Driver for powerpc-as4600-54t
 #
@@ -22,6 +21,8 @@ from switchlight.vendor.accton import *
 
 class SwitchLightPlatformImplementation(SwitchLightPlatformAccton):
 
+    onie_base_address = "0xeff70000"
+
     def model(self):
         return 'AS4600-54T'
 
@@ -29,11 +30,36 @@ class SwitchLightPlatformImplementation(SwitchLightPlatformAccton):
         return 'powerpc-as4600-54t'
 
     def _plat_info_dict(self):
-        raise Exception()
+        return {
+            platinfo.LAG_COMPONENT_MAX : 16,
+            platinfo.PORT_COUNT : 52,
+            }
 
     def _plat_oid_table(self):
-        raise Exception()
+        BSN_SENSORS = '.1.3.6.1.4.1.37538.2.3'
+        return {
+            oids.TEMP_SENSORS : {
+                'ct1'   : BSN_SENSORS + '.1.1.3.1',
+                'ct2'   : BSN_SENSORS + '.1.1.3.2',
+                'ct3'   : BSN_SENSORS + '.1.1.3.3',
+                'ct4'   : BSN_SENSORS + '.1.1.3.4',
+                'ct5'   : BSN_SENSORS + '.1.1.3.5',
+                },
+            oids.CHASSIS_FAN_SENSORS : {
+                'cfan1' : BSN_SENSORS + '.2.1.3.2',
+                'cfan2' : BSN_SENSORS + '.2.1.3.3',
+                },
+            oids.POWER_FAN_SENSORS : {
+                'pfan1' : BSN_SENSORS + '.2.1.3.1',
+                },
 
+            # Not available
+            oids.POWER_SENSORS : {
+                },
+            }
+
+    def get_environment(self):
+        return subprocess.check_output(['/usr/bin/ofad-ctl', 'environment'])
 
 if __name__ == "__main__":
     print SwitchLightPlatformImplementation()
--- components/all/platform-config/powerpc-as5610-52x/src/python/slpc.py
+++ components/all/platform-config/powerpc-as5610-52x/src/python/slpc.py
@@ -8,7 +8,6 @@
 # 
 # </bsn.cl>
 ############################################################
-############################################################
 #
 # Platform Driver for powerpc-as5610-52x
 #
@@ -21,6 +20,8 @@ from switchlight.platform.base import *
 from switchlight.vendor.accton import *
 
 class SwitchLightPlatformImplementation(SwitchLightPlatformAccton):
+
+    onie_base_address = "0xeff70000"
 
     def model(self):
         return "AS5610-52X"
@@ -35,7 +36,34 @@ class SwitchLightPlatformImplementation(SwitchLightPlatformAccton):
             }
 
     def _plat_oid_table(self):
-        raise Exception()
+        BSN_SENSORS = '.1.3.6.1.4.1.37538.2.3'
+        return {
+            oids.TEMP_SENSORS : {
+                'ct1'   : BSN_SENSORS + '.1.1.3.1',
+                'ct2'   : BSN_SENSORS + '.1.1.3.2',
+                'ct3'   : BSN_SENSORS + '.1.1.3.3',
+                'ct4'   : BSN_SENSORS + '.1.1.3.4',
+                'ct5'   : BSN_SENSORS + '.1.1.3.5',
+                'ct6'   : BSN_SENSORS + '.1.1.3.6',
+                'ct7'   : BSN_SENSORS + '.1.1.3.7',
+                'ct8'   : BSN_SENSORS + '.1.1.3.8',
+                'ct9'   : BSN_SENSORS + '.1.1.3.9',
+                },
+            oids.CHASSIS_FAN_SENSORS : {
+                'cfan1' : BSN_SENSORS + '.2.1.3.2',
+                },
+            oids.POWER_FAN_SENSORS : {
+                'pfan1' : BSN_SENSORS + '.2.1.3.1',
+                'pfan2' : BSN_SENSORS + '.2.1.3.3',
+                },
+            oids.POWER_SENSORS : {
+                'power' : BSN_SENSORS + '.4.1.3.3',
+                },
+            }
+
+
+    def get_environment(self):
+        return subprocess.check_output(['/usr/bin/ofad-ctl', 'environment'])
 
 
 if __name__ == "__main__":
