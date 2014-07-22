5a528ca675b5d985f38565c56917c8cbd6178756
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2014-07-22 10:58:56 -0700

    PAN-1002
    Add dumpt6 debug support to release images.

--- builds/swi/powerpc/release-bcf/packages.mk
+++ builds/swi/powerpc/release-bcf/packages.mk
@@ -5,7 +5,8 @@
 include $(SWITCHLIGHT)/builds/swi/powerpc/basepackages.mk
 include platforms.mk
 
-SWI_PACKAGES_ALL += pcli slrest
+SWI_PACKAGES_ALL += pcli slrest t6-dump-flows
+
 SWI_PACKAGES_POWERPC += ofad-6.3.3-release-t5 \
 		libbroadcom-6.3-kernel-85xx-modules \
 		libbroadcom-6.3-kernel-e500mc-modules
