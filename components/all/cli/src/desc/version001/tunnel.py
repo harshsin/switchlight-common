ced6e1bc2d709ec694198fe59fa38cc723932e46
Wilson Ng <wilson.ng@bigswitch.com>
2014-07-24 16:46:31 -0700

    PAN-853: address comments.

--- components/all/cli/src/desc/version001/tunnel.py
+++ components/all/cli/src/desc/version001/tunnel.py
@@ -1,16 +1,36 @@
 # Copyright (c) 2014  BigSwitch Networks
 
+import command
+
+from sl_util import const
+from sl_util.ofad import OFADCtl
+
+def show_tunnel(data):
+    cmd = 'tunnel summary'
+    if 'tunnel-id' in data:
+        cmd += ' %s%d' % (const.TUN_PORT_BASE, data['tunnel-id'])
+    OFADCtl.run(cmd)
+
+command.add_action('implement-show-tunnel', show_tunnel,
+                   {'kwargs': {'data' : '$data',}})
+
 SHOW_TUNNEL_COMMAND_DESCRIPTION = {
     'name'          : 'show',
     'mode'          : 'login',
+    'action'        : 'implement-show-tunnel',
+    'doc'           : 'tunnel|show',
+    'doc-example'   : 'tunnel|show-example',
     'no-supported'  : False,
     'args'          : (
         {
             'token'         : 'tunnel',
-            'action'        : 'ofad-ctl-command',
-            'command'       : 'tunnel summary',
             'short-help'    : 'Show tunnel summary',
-            'doc'           : 'tunnel|show',
+        },
+        {
+            'field'         : 'tunnel-id',
+            'base-type'     : 'integer',
+            'syntax-help'   : 'Tunnel ID',
+            'optional'      : True,
         },
     ),
 }
