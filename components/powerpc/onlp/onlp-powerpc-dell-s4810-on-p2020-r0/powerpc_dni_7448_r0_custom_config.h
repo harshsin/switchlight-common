d2dd58e15638893e08ec379b03c46a208eb2556e
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2014-07-29 09:56:58 -0700

    ONLP package for the new S4810 platform.

---
+++ components/powerpc/onlp/onlp-powerpc-dell-s4810-on-p2020-r0/powerpc_dni_7448_r0_custom_config.h
@@ -0,0 +1,15 @@
+/************************************************************
+ * <bsn.cl fy=2014 v=none>
+ * </bsn.cl>
+ ************************************************************
+ *
+ *
+ *
+ ***********************************************************/
+#ifndef __POWERPC_DNI_7448_R0_CUSTOM_CONFIG_H__
+#define __POWERPC_DNI_7448_R0_CUSTOM_CONFIG_H__
+
+#define POWERPC_DNI_7448_R0_CONFIG_PLATFORM_IDENTIFIER "powerpc-dell-s4810-on-p2020-r0"
+#define POWERPC_DNI_7448_R0_CONFIG_AIM_LOG_MODULE_NAME powerpc_dell_s4810_on_p2020_r0
+
+#endif /* __POWERPC_DNI_7448_R0_CUSTOM_CONFIG_H__ */
