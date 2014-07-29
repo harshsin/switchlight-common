cbaf46d4f0bbd0be097ef2623b7c3ce29a52d454
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2014-07-29 09:35:04 -0700

    Add new DELL S4810 platform.

--- builds/installer/powerpc/ztn/platforms.mk
+++ builds/installer/powerpc/ztn/platforms.mk
@@ -12,6 +12,7 @@ INSTALLER_PLATFORMS :=  powerpc-quanta-lb9-r0 \
 			powerpc-quanta-lb9a-r0 \
 			powerpc-quanta-ly2-r0 \
 	 		powerpc-dni-7448-r0 \
+	 		powerpc-dell-s4810-on-p2020-r0 \
 			powerpc-as6700-32x-r0 \
 			powerpc-as5710-54x-r0a \
 			powerpc-as5710-54x-r0b \
