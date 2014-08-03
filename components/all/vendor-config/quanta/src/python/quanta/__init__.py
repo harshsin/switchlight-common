f34225de39fb717253990839180bf1f9ce9e1abb
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2014-08-03 12:48:01 -0700

    Support onie.json.

--- components/all/vendor-config/quanta/src/python/quanta/__init__.py
+++ components/all/vendor-config/quanta/src/python/quanta/__init__.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 ############################################################
 # <bsn.cl fy=2013 v=none>
-# 
-#        Copyright 2013, 2014 BigSwitch Networks, Inc.        
-# 
-# 
-# 
+#
+#        Copyright 2013, 2014 BigSwitch Networks, Inc.
+#
+#
+#
 # </bsn.cl>
 ############################################################
 ############################################################
@@ -16,13 +16,39 @@
 from switchlight.platform.base import SwitchLightPlatformBase, sysinfo
 import struct
 import time
+import os
+import json
 
 class SwitchLightPlatformQuanta(SwitchLightPlatformBase):
+
 
     def manufacturer(self):
         return "Quanta"
 
+    def _sys_info_dict_onie(self):
+        if os.path.exists("/etc/.onie.json"):
+            od = json.load(file("/etc/.onie.json"))
+            rv = {}
+            rv[sysinfo.PRODUCT_NAME] = od['Product Name']
+            rv[sysinfo.SERIAL_NUMBER] = od['Serial Number']
+            if rv[sysinfo.SERIAL_NUMBER] is None:
+                rv[sysinfo.SERIAL_NUMBER] = "UNKNOWN"
+            rv[sysinfo.MAC_ADDRESS] = od['MAC']
+            rv[sysinfo.PART_NUMBER] = od['Part Number']
+            rv[sysinfo.MANUFACTURE_DATE] = od['Manufacture Date']
+            rv[sysinfo.HARDWARE_VERSION] = od['Label Revision']
+            return rv;
+        else:
+            return None
+
+    def _eeprom_file(self):
+        return None
+
     def _sys_info_dict(self):
+
+        if self._eeprom_file() is None:
+            return self._sys_info_dict_onie()
+
         info = {}
 
         # _eeprom_file() is provided by the derived platform class
