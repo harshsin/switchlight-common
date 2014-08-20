9f42a3da0d60429d1658434aa7ae2491e9714b5d
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2014-08-20 00:14:23 -0700

    Architecture is now lowercase.

--- builds/swi/powerpc/internal-bcf/packages.mk
+++ builds/swi/powerpc/internal-bcf/packages.mk
@@ -7,7 +7,7 @@ include platforms.mk
 
 SWI_PACKAGES_ALL += pcli slrest t6-dump-flows switchlight-internal-dbg-sources
 
-SWI_PACKAGES_POWERPC += \
+SWI_PACKAGES_powerpc += \
 	brcmd-6.3.3-internal \
 	libbroadcom-6.3-internal-dbg \
 	libbroadcom-6.3-kernel-85xx-modules \
