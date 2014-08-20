454db281ac98e9b5ca8f556550a633f7990e85eb
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2014-08-19 18:00:11 -0700

    The architecture is now lowercase.

--- builds/swi/powerpc/release-bt/packages.mk
+++ builds/swi/powerpc/release-bt/packages.mk
@@ -6,7 +6,7 @@ include $(SWITCHLIGHT)/builds/swi/powerpc/basepackages.mk
 include platforms.mk
 
 SWI_PACKAGES_ALL += pcli slrest
-SWI_PACKAGES_POWERPC += ofad-6.3.3-release \
+SWI_PACKAGES_powerpc += ofad-6.3.3-release \
 		libbroadcom-6.3-kernel-85xx-modules \
 		libbroadcom-6.3-kernel-e500mc-modules
 
